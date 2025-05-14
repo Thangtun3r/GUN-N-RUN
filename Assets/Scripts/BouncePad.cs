9d9c1c287d255d8b3e299fff53d53268b6fc3c9f
Thangtuner <voquangthang04@gmail.com>
2025-05-14 22:43:14 +0700

    add jump pad

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,15 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    [SerializeField] private float bounceForce;
+    private Vector2 bounceDirection = Vector2.up;
+    
+    void OnCollisionEnter2D(Collision2D collision) 
+    { 
+        Debug.Log(collision.gameObject.name);
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * bounceForce,ForceMode2D.Impulse);
+        
+    } 
+}
---
+++ Assets/Scripts/IBouncable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IBouncable 
+{
+    void Bounce(Vector2 bounceDirection, float bounceForce);
+}
